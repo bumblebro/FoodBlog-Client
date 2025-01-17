1ec65da80e5a4ca139d7aa043048fbb5d79dd72b
Shreyas M S <shreyasmadappady20@gmail.com>
2025-01-17 12:36:20 +0530

    c

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,17 +16,18 @@ function page() {
         Welcome to SavoryTouch, your go-to destination for all things food and
         drink. Here, we celebrate the joys of culinary exploration, where every
         dish tells a story and every flavor brings an experience to life.
-        Whether you&apos;re a passionate home cook, a curious foodie, or someone who
-        just loves to savor the art of dining, you&apos;ve found the perfect place.
+        Whether you&apos;re a passionate home cook, a curious foodie, or someone
+        who just loves to savor the art of dining, you&apos;ve found the perfect
+        place.
       </p>
 
       <p>
         At SavoryTouch, we believe that food is more than just
-        nourishment&mdash; it&apos;s an adventure for the senses. Our mission is to
-        inspire, inform, and share the love of food by providing a diverse range
-        of recipes, cooking tips, and food culture insights. From easy weeknight
-        dinners and gourmet meals to the latest trends in the culinary world, we
-        have something for every taste.
+        nourishment&mdash; it&apos;s an adventure for the senses. Our mission is
+        to inspire, inform, and share the love of food by providing a diverse
+        range of recipes, cooking tips, and food culture insights. From easy
+        weeknight dinners and gourmet meals to the latest trends in the culinary
+        world, we have something for every taste.
       </p>
 
       <p>
@@ -48,15 +49,15 @@ function page() {
       <p>
         Whether you&rsquo;re here to improve your cooking skills, get inspired
         for your next meal, or simply discover the best new places to eat and
-        drink, SavoryTouch is your companion in the kitchen and beyond. We&apos;re
-        constantly adding fresh, flavorful content that will inspire you to
-        explore, create, and indulge in the richness of food and drink.
+        drink, SavoryTouch is your companion in the kitchen and beyond.
+        We&apos;re constantly adding fresh, flavorful content that will inspire
+        you to explore, create, and indulge in the richness of food and drink.
       </p>
 
       <p>
         Welcome to SavoryTouch&mdash;where every recipe is an adventure, and
-        every meal is an opportunity to savor the moment. Let&apos;s cook, eat, and
-        discover together.
+        every meal is an opportunity to savor the moment. Let&apos;s cook, eat,
+        and discover together.
       </p>
     </div>
   );
