58ce53a7ebb24569e7cbe9e94ed192ca2dbb2977
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-05 10:28:48 +0530

    c

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -4,6 +4,9 @@ import { ImageResponse } from "@vercel/og";
 // export const config = {
 //   runtime: "experimental-edge",
 // };
+export const config = {
+  runtime: "experimental-edge",
+};
 
 export async function GET(req: NextRequest) {
   const { searchParams, protocol, host } = new URL(req.url);
