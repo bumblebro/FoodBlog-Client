b2181ae545d31a9882763c06276ee20d586791e6
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-05 09:50:31 +0530

    c

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: ["/", "/api/og/*"],
       },
     ],
     sitemap: `${process.env.NEXT_PUBLIC_BASE_API_URL}/sitemap.xml`,
