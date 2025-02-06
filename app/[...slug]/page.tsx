61e85dece67df704085c092a261ca6cb45fe12f9
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-06 19:50:22 +0530

    c

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -190,7 +190,7 @@ export async function generateMetadata({ params }: params): Promise<Metadata> {
         openGraph: {
           images: [
             {
-              url: currentPost?.imageurl || "",
+              url: imageUrl || "",
             },
           ],
         },
