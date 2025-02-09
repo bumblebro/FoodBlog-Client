fab89a841f3c26d47875dcad4a0b7f11a0be8c21
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-09 13:26:04 +0530

    c

--- app/api/blogscomplete/BLOGCOMPLETE.ts
+++ app/api/blogscomplete/BLOGCOMPLETE.ts
@@ -7,11 +7,18 @@ const prisma = new PrismaClient();
 export default async function BLOGCOMPLETE() {
   const blogs = await prisma.foodBlogs.findMany({
     select: {
+      author: true, // Name of the author
       section: true,
       subsection: true,
       subsubsection: true,
-      title: true,
-      creationDate: true,
+      title: true, // Title of the page
+      slug: true, // URL-friendly slug for the blog
+      imageurl: true, // URL of the main image
+      content: true, // Short and detailed description of the recipe
+      instructions: true, // Step-by-step instructions for the recipe
+      recipedescription: true,
+      recipedetails: true, // Recipe details for different serving sizes (1X, 2X, 3X, 4X)
+      creationDate: true, // Date the blog was created
     },
 
     // cacheStrategy: { ttl: 86400 },
---
+++ app/feed.xml/route.ts
@@ -0,0 +1,76 @@
+import BLOGCOMPLETE from "@/app/api/blogscomplete/BLOGCOMPLETE";
+import { FoodBlogs } from "@prisma/client";
+import { Feed } from "feed";
+
+export const revalidate = 1; // revalidate at most every hour
+
+const domain =
+  process.env.NEXT_PUBLIC_BASE_API_URL?.replace(/^https:/, "http:") || "";
+
+export async function GET(request: Request, response: Response) {
+  const recipes = await BLOGCOMPLETE();
+  console.log(`rec`, recipes);
+
+  const rss = generateRSSFeed(recipes);
+
+  return new Response(rss, {
+    status: 200,
+    headers: { "Content-Type": "text/xml" },
+  });
+}
+
+function generateRSSFeed(recipes: any) {
+  const siteURL = process.env.NEXT_PUBLIC_BASE_API_URL || "";
+  const date = new Date();
+  const author = {
+    name: "SavoryTouch",
+    link: "https://savorytouch.com",
+  };
+
+  const feed = new Feed({
+    title: "SavoryTouch",
+    description:
+      "Welcome to SavoryTouch, your go-to destination for all things food and drink. Here, we celebrate the joys of culinary exploration where every dish tells a story and every flavor brings an experience to life. Whether you&apos;re a passionate home cook, a curious foodie, or someone who just loves to savor the art of dining, you&apos;ve found the perfect place.",
+    id: siteURL,
+    link: siteURL,
+    image: `${siteURL}/opengraph-image.png`,
+    favicon: `${siteURL}/favicon.ico`,
+    copyright: `All rights reserved ${date.getFullYear()},`,
+    updated: date, // today's date
+    generator: "Feed",
+    feedLinks: {
+      rss2: `${siteURL}/rss/feed.xml`, // xml format
+      json: `${siteURL}/rss/feed.json`, // json fromat
+    },
+    author,
+  });
+
+  recipes.map((r: any, i: any) => {
+    const imageUrl =
+      process.env.NEXT_PUBLIC_BASE_API_URL +
+      `/api/og?` +
+      `title=${r.title}` +
+      `&cover=${r.imageurl}`;
+    console.log(`urllll`, imageUrl);
+
+    const url = siteURL + "/" + r.slug;
+    if (imageUrl) {
+      feed.addItem({
+        title: r.name,
+        id: url,
+        link: url,
+        description: r.recipedescription,
+        content: r.description,
+        author: [author],
+        contributor: [author],
+        date: r.creationDate,
+        image: {
+          type: "image/png",
+          url: domain + `/api/og?title=${r.title}&amp;cover=${r.imageurl}`,
+        },
+      });
+    }
+  });
+
+  return feed.rss2();
+}
