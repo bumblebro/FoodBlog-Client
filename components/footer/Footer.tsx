ba346766607b68e07bea476bb9da1efc285ee3e6
Shreyas M S <shreyasmadappady20@gmail.com>
2025-04-24 22:29:00 +0530

    c

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -31,7 +31,7 @@ function Footer() {
     <div
       className={`bg-[#F4F2F2] text-[#000000] font-sans ${Poppins400.className}`}
     >
-      <div className=" w-full h-[100px]">
+      <div className="bg-gray-200 w-full h-[100px]">
         {" "}
         <GoogleAdUnit>
           {/* Ads1 */}
