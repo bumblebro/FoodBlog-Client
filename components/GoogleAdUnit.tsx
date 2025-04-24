7055c4a40eecbff96ff19852c527245049a8f2c2
Shreyas M S <shreyasmadappady20@gmail.com>
2025-04-25 00:40:59 +0530

    c

--- components/GoogleAdUnit.tsx
+++ components/GoogleAdUnit.tsx
@@ -1,16 +1,35 @@
+// "use client";
+
+// import { ReactNode, Suspense } from "react";
+// import GoogleAdUnitClient from "./GoogleAdUnitClient";
+
+// function GoogleAdUnit({ children }: { children: ReactNode }) {
+//   return (
+//     <Suspense>
+//       <GoogleAdUnitClient isProduction={process.env.NODE_ENV == "production"}>
+//         {children}
+//       </GoogleAdUnitClient>
+//     </Suspense>
+//   );
+// }
+
+// export default GoogleAdUnit;
+
 "use client";
+import React, { ReactNode } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
-import { ReactNode, Suspense } from "react";
-import GoogleAdUnitClient from "./GoogleAdUnitClient";
-
-function GoogleAdUnit({ children }: { children: ReactNode }) {
-  return (
-    <Suspense>
-      <GoogleAdUnitClient isProduction={process.env.NODE_ENV == "production"}>
-        {children}
-      </GoogleAdUnitClient>
-    </Suspense>
-  );
-}
+const GoogleAdUnit = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  React.useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error(err);
+    }
+  }, [pathname, searchParams]);
+  return <React.Fragment>{children}</React.Fragment>;
+};
 
 export default GoogleAdUnit;
--- components/GoogleAdUnitClient.tsx
+++ components/GoogleAdUnitClient.tsx
@@ -1,25 +1,54 @@
+// "use client";
+
+// import { usePathname, useSearchParams } from "next/navigation";
+// import { Fragment, ReactNode, useEffect } from "react";
+
+// function GoogleAdUnitClient({
+//   isProduction,
+//   children,
+// }: {
+//   isProduction: boolean;
+//   children: ReactNode;
+// }) {
+//   const pathName = usePathname();
+//   const searchParams = useSearchParams();
+
+//   useEffect(() => {
+//     if (isProduction) {
+//       (window.adsbygoogle = window.adsbygoogle || []).push({});
+//     } else console.log("Currently in Development");
+//   }, [pathName, searchParams, isProduction]);
+
+//   return <Fragment>{children}</Fragment>;
+// }
+
+// export default GoogleAdUnitClient;
+
 "use client";
-
+import React from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Fragment, ReactNode, useEffect } from "react";
 
-function GoogleAdUnitClient({
-  isProduction,
-  children,
-}: {
-  isProduction: boolean;
-  children: ReactNode;
-}) {
-  const pathName = usePathname();
-  const searchParams = useSearchParams();
+export type GoogleAdUnitProps = {
+  children: React.ReactNode;
+};
 
-  useEffect(() => {
-    if (isProduction) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } else console.log("Currently in Development");
-  }, [pathName, searchParams, isProduction]);
-
-  return <Fragment>{children}</Fragment>;
+declare global {
+  interface Window {
+    adsbygoogle?: any | any[];
+  }
 }
+
+const GoogleAdUnitClient: React.FC<GoogleAdUnitProps> = ({ children }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  React.useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error(err);
+    }
+  }, [pathname, searchParams]);
+  return <React.Fragment>{children}</React.Fragment>;
+};
 
 export default GoogleAdUnitClient;
