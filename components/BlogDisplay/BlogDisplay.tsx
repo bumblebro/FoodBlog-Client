3869cbb4986680b8d72f932be556d723b0be420b
Shreyas M S <shreyasmadappady20@gmail.com>
2025-04-25 11:57:13 +0530

    c

--- components/BlogDisplay/BlogDisplay.tsx
+++ components/BlogDisplay/BlogDisplay.tsx
@@ -19,7 +19,6 @@ import localFont from "next/font/local";
 
 import { Poppins } from "next/font/google";
 import GoogleAdUnit from "../GoogleAdUnit";
-import GoogleAdStart from "../GoogleAdStart";
 // Poppins
 const Poppins700 = Poppins({
   weight: "700",
@@ -328,8 +327,7 @@ function BlogDisplay({ decodedslug, currentPost, posts, latposts }: BlogDisp) {
           <h1 className="px-4 py-4 my-4 italic ">{currentPost.quote}</h1>
         </div>{" "}
         <div className=" py-8 px-4  hidden md:flex md:flex-col">
-          <GoogleAdStart>
-            {" "}
+          <GoogleAdUnit>
             <ins
               className="adsbygoogle"
               style={{ display: "block" }}
@@ -338,17 +336,7 @@ function BlogDisplay({ decodedslug, currentPost, posts, latposts }: BlogDisp) {
               data-ad-format="auto"
               data-full-width-responsive="true"
             />
-          </GoogleAdStart>
-          {/* <GoogleAdUnit>
-            <ins
-              className="adsbygoogle"
-              style={{ display: "block" }}
-              data-ad-client="ca-pub-5012580427673167"
-              data-ad-slot={3048648789}
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            />
-          </GoogleAdUnit> */}
+          </GoogleAdUnit>
           <h1
             className={`text-2xl font-semibold text-center py-4 mb-4 ${Poppins700.className} bg-[#8D6271]  text-white rounded-md`}
           >
@@ -498,7 +486,7 @@ function BlogDisplay({ decodedslug, currentPost, posts, latposts }: BlogDisp) {
         </div>{" "}
       </div>
       <div className="py-8 px-4 md:bg-[#ffffff] xl:pr-0">
-        <GoogleAdStart>
+        <GoogleAdUnit>
           <ins
             className="adsbygoogle"
             style={{ display: "block" }}
@@ -507,17 +495,7 @@ function BlogDisplay({ decodedslug, currentPost, posts, latposts }: BlogDisp) {
             data-ad-format="auto"
             data-full-width-responsive="true"
           />
-        </GoogleAdStart>
-        {/* <GoogleAdUnit>
-          <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-5012580427673167"
-            data-ad-slot={3048648789}
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          />
-        </GoogleAdUnit> */}
+        </GoogleAdUnit>
         <h1
           className={`text-lg font-semibold text-center py-4 mb-4 ${Poppins700.className}  bg-[#8D6271]  text-white rounded-md`}
         >
--- components/GoogleAdStart.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import GoogleAdUnit from "./GoogleAdUnit";
-import { GoogleAdUnitProps } from "./GoogleAdUnitClient";
-
-function GoogleAdStart({ children }: GoogleAdUnitProps) {
-  return <GoogleAdUnit>{children}</GoogleAdUnit>;
-}
-
-export default GoogleAdStart;
