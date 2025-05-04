b03e399735b81f0bca4caac9d5a846a9b677e2b6
Shreyas M S <shreyasmadappady20@gmail.com>
2025-05-04 21:12:52 +0530

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,26 @@ export default function RootLayout({
           ></Script>
         )} */}
         <AdsenseScript />
+        <script
+          data-grow-initializer=""
+          dangerouslySetInnerHTML={{
+            __html: `
+      !(function() {
+        window.growMe || (window.growMe = function(e) {
+          window.growMe._.push(e);
+        }, window.growMe._ = []);
+        var e = document.createElement("script");
+        e.type = "text/javascript";
+        e.src = "https://faves.grow.me/main.js";
+        e.defer = true;
+        e.setAttribute("data-grow-faves-site-id", "U2l0ZTpkMzBhMjJmYi01Y2UzLTQ5ZTQtYjBkNC0yMjEwZjgyMGM4M2E=");
+        
+        var t = document.getElementsByTagName("script")[0];
+        t.parentNode.insertBefore(e, t);
+      })();
+    `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {/* {process.env.NODE_ENV == "production" && (
